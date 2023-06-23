77706c14b8cbab85c3075df7a63560cb0b2b7e4d
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-06-22 23:24:28 -0300

    add consumo api_benef

--- src/api/user_request.js
+++ src/api/user_request.js
@@ -1,7 +1,42 @@
 import { Component } from "react";
 
 class UsuarioController {
+    authUser = async (login,senha) => {
+      
+        try {
 
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+            myHeaders.append("Cookie", "cookiesession1=678A3E195EE55E34037D53FB092A2B01");
+
+            var urlencoded = new URLSearchParams();
+            urlencoded.append("login", "44266737807");
+            urlencoded.append("senha", "unimed");
+
+            const uri = 'https://api-benef-hml.unimednatal.com.br/usuarios/logar'
+            const response = await fetch(uri, {
+                method: 'POST',
+                headers:  myHeaders,
+                body: urlencoded
+            });
+
+            console.log("User Autenticado: ")
+
+            console.log(response.status);
+
+            if (response.status == 200) {
+                const credentials = await response.json();
+                console.log(credentials);
+                return credentials;
+            } else {
+                return null;
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
 
 }
 
